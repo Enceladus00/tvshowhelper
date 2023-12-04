dcb9e3fd038e4ecd4f5758a509dc7fd415e4b654
Enceladus00 <116817356+Enceladus00@users.noreply.github.com>
2023-12-04 14:56:03 +0300

    Update enceapp.py

--- enceapp.py
+++ enceapp.py
@@ -3,18 +3,23 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from encecolormaps import palette
+import imdb
+
+ia = imdb.Cinemagoer()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-custom_palette = palette.palettes['20bnw']
+custom_palette = palette.palettes['redngreen']
 csv_file_path = 'datasets/top250sorted.csv'
 
 df = pd.read_csv(csv_file_path)
 series_list = df['Title'].unique().tolist()
 
+
 @app.route('/')
 def index():
     return render_template('index.html', series_list=series_list)
+
 
 @app.route('/result', methods=['POST'])
 def result():
@@ -22,6 +27,10 @@ def result():
     user_choice = request.form['user_choice']
 
     selected_df = df[df['Title'] == selected_show]
+
+    movies = ia.search_movie(selected_show)
+    selected_series = movies[0]
+    id = selected_series.movieID
 
     if user_choice.lower() == 'heatmap':
         heatmap_data = selected_df.pivot(index='Season', columns='Episode', values='Rating')
@@ -34,12 +43,18 @@ def result():
         plt.close()
         highest_rated_episode = selected_df[selected_df['Rating'] == selected_df['Rating'].max()]['Episode'].iloc[0]
         lowest_rated_episode = selected_df[selected_df['Rating'] == selected_df['Rating'].min()]['Episode'].iloc[0]
-        highest_rated_episode_season = selected_df[selected_df['Rating'] == selected_df['Rating'].max()]['Season'].iloc[0]
-        lowest_rated_episode_season = selected_df[selected_df['Rating'] == selected_df['Rating'].min()]['Season'].iloc[0]
-        return render_template('result.html', img_path=img_path, selected_show=selected_show, highest_rated_episode=highest_rated_episode, lowest_rated_episode=lowest_rated_episode, highest_rated_episode_season=highest_rated_episode_season, lowest_rated_episode_season=lowest_rated_episode_season)
+        highest_rated_episode_season = selected_df[selected_df['Rating'] == selected_df['Rating'].max()]['Season'].iloc[
+            0]
+        lowest_rated_episode_season = selected_df[selected_df['Rating'] == selected_df['Rating'].min()]['Season'].iloc[
+            0]
+
+        return render_template('result.html', img_path=img_path, selected_show=selected_show,
+                               highest_rated_episode=highest_rated_episode, lowest_rated_episode=lowest_rated_episode,
+                               highest_rated_episode_season=highest_rated_episode_season,
+                               lowest_rated_episode_season=lowest_rated_episode_season, id=id)
     elif user_choice.lower() == 'graph':
         plt.figure(figsize=(12, 6))
-        sns.lineplot(x='Episode', y='Rating', hue='Season', data=selected_df, marker='o', palette='viridis')
+        sns.lineplot(x='Episode', y='Rating', hue='Season', data=selected_df, marker='o', palette='inferno_r')
 
         plt.title(f'{selected_show} Ratings Graph')
         plt.xlabel('Episode')
@@ -51,6 +66,7 @@ def result():
         return render_template('result.html', img_path=img_path)
     else:
         return "Invalid choice. Please enter 'heatmap' or 'graph'."
+
 
 if __name__ == '__main__':
     app.run(debug=True)
